c02fdbf6924f22f3701ea4558caa2732b6e486c8
PatrickBly <89408738+PatrickBly@users.noreply.github.com>
2022-03-01 18:21:40 -0500

    Add files via upload

---
+++ unitCircle.js
@@ -0,0 +1,115 @@
+function displayCircle(){
+    
+}
+
+function thirty(){
+    let thirtyDegrees = document.createElement('h2')
+    thirtyDegrees.innerHTML = "30'" 
+    document.appendChild(thirtyDegree)
+    document.getElementById('thirty').style.display = grid
+}
+
+function fourtyFive(){
+    let fourtyFiveDegrees = document.createElement('h2')
+    fourtyFiveDegrees.innerHTML = "45'" 
+    document.appendChild(fourtyFiveDegrees)
+    document.getElementById('fourtyFiveDegrees').style.display = grid
+}
+
+function sixty(){
+    let sixtyDegrees = document.createElement('h2')
+    sixtyDegrees.innerHTML = "60'" 
+    document.appendChild(sixtyDegree)
+    document.getElementById('sixty').style.display = grid
+}
+
+function ninety(){
+    let ninetyDegrees = document.createElement('h2')
+    ninetyDegrees.innerHTML = "90'" 
+    document.appendChild(ninetyDegree)
+    document.getElementById('ninety').style.display = grid
+}
+
+function oneTwenty(){
+    let oneTwentyDegrees = document.createElement('h2')
+    oneTwentyDegrees.innerHTML = "125'" 
+    document.appendChild(oneTwentyDegrees)
+    document.getElementById('oneTwenty').style.display = grid
+}
+
+function oneThirtyFive(){
+    let oneThirtyFiveDegrees = document.createElement('h2')
+    oneThirtyFiveDegrees.innerHTML = "135'" 
+    document.appendChild(oneThirtyFiveDegrees)
+    document.getElementById('oneThirtyFive').style.display = grid
+}
+
+function oneFifty(){
+    let oneFiftyDegrees = document.createElement('h2')
+    oneFiftyDegrees.innerHTML = "150'" 
+    document.appendChild(oneFiftyDegrees)
+    document.getElementById('oneFifty').style.display = grid
+}
+
+function oneEighty(){
+    let oneEightyDegrees= document.createElement('h2')
+    oneEightyDegrees.innerHTML = "180'" 
+    document.appendChild(oneEightyDegrees)
+    document.getElementById('oneEighty').style.display = grid
+}
+
+function twoTen(){
+    let twoTenDegrees = document.createElement('h2')
+    Degree.innerHTML = "210'"
+    document.appendChild(twoTenDegrees)
+    document.getElementById('twoTen').style.display = grid
+}
+
+function twoTwentyFive(){
+    let twoTwentyFiveDegrees = document.createElement('h2')
+    twoTwentyFiveDegrees.innerHTML = "'" 
+    document.appendChild(twoTwentyFiveDegrees)
+    document.getElementById('twoTwentyFiveDegrees').style.display = grid
+}
+
+function twoFourty(){
+    let twoFourtyDegrees = document.createElement('h2')
+    twoFourtyDegrees.innerHTML = "240'" 
+    document.appendChild(twoFourtyDegrees)
+    document.getElementById('twoFourty').style.display = grid
+}
+
+function twoSeventy(){
+    let twoSeventyDegrees = document.createElement('h2')
+    twoSeventyDegrees.innerHTML = "270'" 
+    document.appendChild(twoSeventyDegrees)
+    document.getElementById('twoSeventy').style.display = grid
+}
+
+function threeHundred(){
+    let threeHundredDegrees = document.createElement('h2')
+    threeHundredDegrees.innerHTML = "300'" 
+    document.appendChild(threeHundredDegrees)
+    document.getElementById('threeHundred').style.display = grid
+}
+
+function threeThirty(){
+    let threeThirtyDegrees = document.createElement('h2')
+    threeThirtyDegrees.innerHTML = "330'" 
+    document.appendChild(threeThirtyDegrees)
+    document.getElementById('threeThirty').style.display = grid
+}
+
+function threeFourtyFive(){
+    let threeFourtyFiveDegrees = document.createElement('h2')
+    threeFourtyFiveDegrees.innerHTML = "345'" 
+    document.appendChild(threeFourtyFiveDegrees)
+    document.getElementById('threeFourtyFive').style.display = grid
+}
+
+function threeSixty(){
+    let threeSixtyDegrees = document.createElement('h2')
+    threeSixtyDegrees.innerHTML = "360'" 
+    document.appendChild(threeSixtyDegrees)
+    document.getElementById('threeSixty').style.display = grid
+}
